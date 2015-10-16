b37313bccf79561c4d25cda119074161d922c29d
Gabe Berke-Williams <gabebw@gabebw.com>
2015-10-15 21:33:19 -0700

    working

--- ParseString.hs
+++ ParseString.hs
@@ -14,46 +14,24 @@ import Text.ParserCombinators.Parsec (try)
 data Chunk = Chunk { left :: String, right :: String }
     deriving Show
 
+-- runhaskell ParseString.hs "horse before the heart right"
 main :: IO ()
 main = do
     a <- getArgs
     case a of
-      [str] -> either print print $ parse (parseAround "heart") "" str
+      [w, str] -> either print (print . reconstruct w) $ parse myParser "" str
       _ -> error "please pass one argument with the string to parse"
 
-myParser :: Parser Integer
-myParser = num
+myParser :: Parser Chunk
+myParser = choice $ map (try . parseAround) ["heart", "cart"]
 
-num :: Parser Integer
-num = do
-    n <- lexeme $ many1 digit
-    return (read n)
-
--- Parse `p` followed by whitespace, and discard the whitespace
-lexeme :: Parser a -> Parser a
-lexeme p = p <* whitespace
-
--- Parses (and then ignores) any whitespace
-whitespace :: Parser ()
-whitespace = void $ many $ oneOf " \n\t"
-
-rhymes :: Parser String
-rhymes = choice [string "art", string "cart"]
-
--- Parse before and after a word
+-- Parse before/after a word into a Chunk
+parseAround :: String -> Parser Chunk
 parseAround word = do
     before <- manyTill anyToken (lookAhead $ try $ (void $ string word))
     string word
     after <- many anyToken
     return $ Chunk before after
 
--- runhaskell ParseString.hs "horse before the heart right"
--- Try on "hey there heart hey"
-ugh :: Parser String
-ugh = do
-    manyTill (anyChar) (try $ string "heart")
-    whitespace
-    x <- string "heart"
-    whitespace
-    return x
-    try (many anyChar)
+reconstruct :: String -> Chunk -> String
+reconstruct word (Chunk before after) = before ++ word ++ after
